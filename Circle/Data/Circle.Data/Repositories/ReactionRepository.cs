2633becc9a223af5cc8e4440b23fd3d344b9ee82
assyagr <assyagr@gmail.com>
2025-01-16 18:18:58 +0200

    added repos for models circle.data

---
+++ Circle/Data/Circle.Data/Repositories/ReactionRepository.cs
@@ -0,0 +1,17 @@
+﻿using Circle.Data.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle.Data.Repositories
+{
+	public class ReactionRepository : MetadataBaseGenericRepository<Reaction>
+	{
+		public ReactionRepository(CircleDbContext _dbContext, IHttpContextAccessor httpContextAccessor) : base(_dbContext, httpContextAccessor)
+		{
+		}
+	}
+}
