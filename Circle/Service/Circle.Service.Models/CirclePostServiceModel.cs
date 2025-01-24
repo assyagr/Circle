af9b71836f7db0b71aebc1fe6b71a50eeb1e2aff
assyagr <assyagr@gmail.com>
2025-01-24 22:12:04 +0200

    comment, reaction, post service models

---
+++ Circle/Service/Circle.Service.Models/CirclePostServiceModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle.Service.Models
+{
+	public class CirclePostServiceModel : MetadataBaseServiceModel
+	{
+		public List<AttachmentServiceModel> Content { get; set; }
+
+		public string Caption { get; set; }
+
+		public List<UserPostReactionServiceModel> Reactions { get; set; }
+
+		public List<UserPostCommentServiceModel> Comments { get; set; }
+
+		public List<CircleUserServiceModel> TaggedUsers { get; set; }
+
+		public bool IsDeleted { get; set; }
+	}
+}
