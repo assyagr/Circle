d3f8eb7008753f720977895250ae547a09591858
assyagr <assyagr@gmail.com>
2025-01-24 22:57:32 +0200

    added UserProfile model and service model

--- Circle/Data/Circle.Data.Models/CircleUser.cs
+++ Circle/Data/Circle.Data.Models/CircleUser.cs
@@ -14,27 +14,14 @@ namespace Circle.Data.Models
         // also SecurityStamp 
         //Example Id for reference: e639a8c5-d9ab-43ec-81a3-e3f49a5c9ead
         public string DisplayName { get; set; }
-		public string Bio {  get; set; }	
-		public Attachment ProfilePictureCloudUrl { get; set; }
+        
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow; //time apon creation 
 
-        public string TypeOfAurdiance { get; set; } //who he is (audiance)
+        //public string TypeOfAurdiance { get; set; } //who he is (audiance) ???
 
         public string CricleRole { get; set; } //Admin, User, Mod ??
 
         public bool IsDeleted { get; set; } // soft delete 
-
-
-
-
-
-
-
-
-        public List<CircleUser> Friends { get; set; }
-        public List<CircleUser> Followers { get; set; }
-		public List<CircleUser> Following { get; set; }
-        public List<CirclePost> Posts { get; set; } //Posts by user
 
         //Suggestion:
         //public int GroupCricle {  get; set; } // App circle to have friend cricles/Groups but diffrent ig Ui is a circle :p
