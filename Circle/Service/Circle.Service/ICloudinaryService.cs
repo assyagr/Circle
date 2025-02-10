ccdad41e7ea662e9747430f71b86129395fee509
Simeon Bogoev <monybogoev@gmail.com>
2025-02-10 14:00:06 +0200

    "CloudUrl" setup, but not implemented anywhere. (Cloudinary)

---
+++ Circle/Service/Circle.Service/ICloudinaryService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle.Service
+{
+    public interface ICloudinaryService
+    {
+
+        Task<Dictionary<string, object>> UploadFile(IFormFile file);
+    }
+}
