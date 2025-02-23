dc091ed75d4e50dea4be368164e5c365fc2e1278
Valx7xx <163676393+Valx7xx@users.noreply.github.com>
2025-02-23 09:25:06 +0200

    Add files via upload

---
+++ Circle/Service/Circle.Service.Mappings/FriendshipMappings.cs
@@ -0,0 +1,37 @@
+﻿using Circle.Data.Models;
+using Circle.Service.Friends;
+using Circle.Service.Models;
+
+namespace Circle.Service.Mappings
+{
+    public static class FriendshipMappings
+    {
+
+        public static FriendRequest? ToEntity(this FriendshipServiceModel model)
+        {
+            if (model == null)
+                return null;
+
+            return new FriendRequest
+            {
+                Id = model.Id,
+                SenderId = model.SenderId,
+                ReceiverId = model.ReceiverId
+            };
+        }
+
+     
+        public static FriendshipServiceModel? ToModel(this FriendRequest entity)
+        {
+            if (entity == null)
+                return null;
+
+            return new FriendshipServiceModel
+            {
+                Id = entity.Id,
+                SenderId = entity.SenderId,
+                ReceiverId = entity.ReceiverId
+            };
+        }
+    }
+}
