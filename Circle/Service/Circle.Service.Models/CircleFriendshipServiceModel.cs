c5d28d1d0397bb503bf716cd5d6f7f820c7edd95
Valx7xx <163676393+Valx7xx@users.noreply.github.com>
2025-02-23 09:25:38 +0200

    Add files via upload

---
+++ Circle/Service/Circle.Service.Models/CircleFriendshipServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using Circle.Data.Models;  
+using Circle.Service.Models;  
+
+namespace Circle.Service.Models
+{
+    public class FriendRequestServiceModel
+    {
+        public int Id { get; set; }
+        public required string SenderId { get; set; }
+        public required string ReceiverId { get; set; }
+    }
+}
